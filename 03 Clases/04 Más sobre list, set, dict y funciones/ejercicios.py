ae22cef97471b609da73b6845706be7d19c5c32b
Fernando-JAL <alcantaralopezfernando@gmail.com>
2024-09-03 19:04:13 -0600

    Notas de clase Sep-03

---
+++ 03 Clases/04 Más sobre list, set, dict y funciones/ejercicios.py
@@ -0,0 +1,11 @@
+# 1) Create a function that get an integer and calculate the factorial of
+# that number (a non-negative integer)
+
+# 2) Create a function that receives 3 numbers and verify if the third number is
+# between the 2 first numbers
+
+# 3) Given a string, create 2 functions to count the number of spaces in the string
+
+# 4) Given a list, create a function to print the odd numbers in the list
+print('_'.isalpha())
+print('9'.isdigit())
