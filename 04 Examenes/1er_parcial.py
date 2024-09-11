b8950776d214a117be6a4b3eba148912cc3fa6f6
Fernando-JAL <alcantaralopezfernando@gmail.com>
2024-09-10 18:20:52 -0600

    Clase 09-10

--- 04 Examenes/1er_parcial.py
+++ 04 Examenes/1er_parcial.py
@@ -8,10 +8,27 @@
 
 # ESCRIBE TU CÓDIGO AQUÍ
 
+def fibonacci(n: int) -> int:
+    if n < 1:
+        print('fibonacci solo es válido para n >= 1')
+        return 0
+    if n <= 2:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
 
+def fibonacci2(n):
+    if (n == 1) or (n == 2):
+        return 1
+    numeros = [1, 1]
+    for i in range(2, n):
+        fib = numeros[i-1] + numeros[i-2]
+        numeros.append(fib)
+    return numeros[-1]
 
+# print(fibonacci2(-5))
 
-
+# valor = int(input('Introduce un entero n, para calcular el n-ésimo término de fibonacci: '))
+# print(fibonacci(valor))
 
 
 # 2. Utilizando la función del ejercico anterior, recibir por teclado un entero $n$ e imprimir los $n$-ésimos términos de
@@ -19,6 +36,9 @@
 
 # ESCRIBE TU CÓDIGO AQUÍ
 
+# valores = int(input('Introduce un entero n, para calcular los n términos de fibonacci: '))
+# for valor in range(1, valores+1):
+#     print(fibonacci(valor))
 
 
 
@@ -26,18 +46,22 @@
 
 
 # 3. A Watson le gusta retar la habilidad matemática de Sherlock. Watson proveerá un valor inicial y un valor final que
-# describa el rango de enteros. Sherlos debe determinar el número de cuadrados enteros dentro del rango.
+# describa el rango de enteros. Sherlock debe determinar el número de cuadrados enteros dentro del rango.
 #
 # Nota: Un cuadrado entero es un entero cuya raíz es un entero, ej 4, 9, 16.
 #
-# - Crear una función que reciba dos enteros: $a$ y $b$ y retorne el número d ecuadrados enteros en el rango $[a, b]$
+# - Crear una función que reciba dos enteros: $a$ y $b$ y retorne el número de cuadrados enteros en el rango $[a, b]$
 
 # ESCRIBE TU CÓDIGO AQUÍ
 
+def cuadrados_enteros(a, b):
+    contador = 0
+    for val in range(a, b+1):
+        if val**0.5 % 1 == 0:
+            contador += 1
+    return contador
 
-
-
-
+# print(cuadrados_enteros(4, 25))
 
 
 # 4. Sea una cadena $s$ de letras en minúscula q se repite infinitamente. Dado un entero $n$, encuentra e
@@ -49,10 +73,17 @@
 
 # ESCRIBE TU CÓDIGO AQUÍ
 
+def cadena_infinita(s, n):
+    longitud = len(s)
 
+    multiplos = (n//longitud + 1)
+    cadena_extendida = s * multiplos
 
+    return cadena_extendida[:n].count('a')
 
-
+s, n_char = input('Introduce la cadena y el entero:').split()
+n = int(n_char)
+print(cadena_infinita(s, n))
 
 
 # 5. Dada una lista de enteros que representan las longitudes de bastones, debes iterativamente cortar los bastones en
