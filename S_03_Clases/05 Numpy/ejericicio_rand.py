210595ab1770a27a0ccf8c5f102c0b012968c599
Fernando-JAL <alcantaralopezfernando@gmail.com>
2024-09-18 18:55:17 -0600

    notas de 09-18_part 2

--- S_03_Clases/05 Numpy/ejericicio_rand.py
+++ S_03_Clases/05 Numpy/ejericicio_rand.py
@@ -1,11 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # Generar números aleatorios con distribución normal (0, 1)
 # array uniforme con curva gaussiana
 arreglo = np.random.normal(size=1000000)
-plt.hist(arreglo, bins=100)
-plt.show()
+# plt.hist(arreglo, bins=100)
+# plt.show()
 
 # Ejercicio. Transformar los datos con distribución normal (0, 1) a
 # distribución normal (5, 2)
+lista = []
+mu, sigma = 5, 2
+for valor in arreglo:
+    nuevo_valor = valor * sigma + mu
+    lista.append(nuevo_valor)
+
+plt.hist(arreglo*sigma + mu, bins=100)
+plt.show()
